2f812ef10e84daa521aa25fb7783fde506afb3e5
saurav13152 <chatterjee.saurav123@gmail.com>
2021-08-04 21:13:36 +0530

    THA done

---
+++ Front end/Day-23/hooks/src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import "./App.css";
+
+const App = () => {
+  const state = useState();
+  const [count, setCount] = useState(1);
+
+  //using hooks
+
+  const IncNum = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <>
+      <h1> {count}</h1>
+      <button onClick={IncNum}>Click Here</button>
+    </>
+  );
+};
+export default App;
